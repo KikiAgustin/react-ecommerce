45ec298055dcd4bd9f9e868a1c28036127043b4a
kikiAgustin <kikiagustin62@gmail.com>
2022-09-14 07:17:37 +0600

    add page about

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Page!</h1>
+            <Link href='/' >
+                <a >kembali ke halaman home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default About;
